7934cd446b7c676f823291971935499171c1396a
budde25 <ethan@ebudd.io>
2020-10-10 15:01:21 -0500

    Adds somes test and fixes a bug for http

--- src/http.rs
+++ src/http.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 use url::Url;
 
 pub const GITHUB_URL: &str = "https://github.com/";
-pub const GITLAB_URL: &str = "https://gitlab.com)";
+pub const GITLAB_URL: &str = "https://gitlab.com/";
 
 /// creates the client
 fn get_client() -> Result<Client, Error> {
@@ -31,5 +31,44 @@ pub async fn get_standard(username: &str, url: Url) -> Result<String, Error> {
     match response {
         Ok(resp) => return Ok(resp.text().await?),
         Err(e) => return Err(e),
+    }
+}
+
+// TESTS
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+
+    #[test]
+    #[ignore]
+    fn get_github_budde25() {
+        let url = Url::parse(GITHUB_URL).unwrap();
+        get_standard("budde25", url)
+        .expect("Args are valid should return a result");
+    }
+
+    #[test]
+    #[ignore]
+    fn get_gitlab_budde25() {
+        let url = Url::parse(GITLAB_URL).unwrap();
+        get_standard("budde25", url)
+        .expect("Args are valid should return a result");
+    }
+
+    #[test]
+    #[ignore]
+    fn get_wisc_gitlab_budd() {
+        let url = Url::parse("https://gitlab.cs.wisc.edu").unwrap();
+        get_standard("budd", url)
+        .expect("Args are valid should return a result");
+    }
+
+    #[test]
+    #[ignore]
+    fn get_invalid_url() {
+        let url = Url::parse("https://abc.edu").unwrap();
+        get_standard("budd", url)
+        .expect_err("Args are valid should not return a result");
     }
 }
